0a997d9ab70ea63c831db9f02fcc7fbefa546bb9
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-10 06:27:28 -0300

    ✨ feat: initial plop generator structure

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -11,18 +11,28 @@ module.exports = (plop) => {
     actions: [
       {
         type: 'add',
-        path: './generated/components/{{pascalCase name}}/index.tsx',
-        templateFile: 'templates/Component.tsx.hbs'
+        path: '../packages/shared-ui/src/lib/components/{{pascalCase name}}/{{pascalCase name}}.index.ts',
+        templateFile: 'templates/component/index.ts.hbs'
       },
       {
         type: 'add',
-        path: './generated/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
+        path: '../packages/shared-ui/src/lib/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+        templateFile: 'templates/component/example.spec.tsx.hbs'
       },
       {
         type: 'add',
-        path: './generated/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
+        path: '../packages/shared-ui/src/lib/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/component/example.stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../packages/shared-ui/src/lib/components/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
+        templateFile: 'templates/component/example.styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../packages/shared-ui/src/lib/components/{{pascalCase name}}/{{pascalCase name}}.component.tsx',
+        templateFile: 'templates/component/example.component.tsx.hbs'
       }
     ]
   })
