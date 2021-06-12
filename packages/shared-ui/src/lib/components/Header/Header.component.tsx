48720d1cc74a86f6bfff5e696fab810ff59f89da
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-12 07:46:00 -0300

    🎉 fix: now its fixed!

--- packages/shared-ui/src/lib/components/Header/Header.component.tsx
+++ packages/shared-ui/src/lib/components/Header/Header.component.tsx
@@ -1,14 +1,13 @@
 import * as S from './Header.styles'
 
 export interface HeaderProps {
-  color?: 'lightGray' | 'primary'
   testId?: string
 }
 
-export const Header = ({ color = 'lightGray', testId = 'Header' }: HeaderProps) => {
+export const Header = ({ testId = 'Header' }: HeaderProps) => {
   return (
     <S.Container>
-      <S.Wrapper color={color} data-testid={testId}>
+      <S.Wrapper data-testid={testId}>
         <S.Header>Paulo Luan é o brabo</S.Header>
       </S.Wrapper>
     </S.Container>
--- packages/shared-ui/src/lib/components/Header/Header.styles.ts
+++ packages/shared-ui/src/lib/components/Header/Header.styles.ts
@@ -3,15 +3,15 @@ import styled, { css } from 'styled-components'
 import { HeaderProps } from '.'
 
 export const Container = styled.div<HeaderProps>`
-  ${({ theme, color }) => css`
+  ${({ theme }) => css`
     height: 100px;
     width: 100px;
   `}
 `
 
 export const Wrapper = styled.div<HeaderProps>`
-  ${({ theme, color }) => css`
-    background-color: ${theme.colors[color]};
+  ${({ theme }) => css`
+    background-color: black;
   `}
 `
 export const Header = styled.h1`
