91eef66685927ca019f33cb5b13399d64f21fb17
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-17 05:51:28 -0300

    🎨 style: added initial styles for transaction table

--- packages/shared-ui/src/lib/components/money/TransactionTable/TransactionTable.component.tsx
+++ packages/shared-ui/src/lib/components/money/TransactionTable/TransactionTable.component.tsx
@@ -9,9 +9,40 @@ export const TransactionTable = ({
 }: TransactionTableProps) => {
   return (
     <S.Container>
-      <S.Wrapper data-testid={testId}>
-        <S.Main>TransactionTable</S.Main>
-      </S.Wrapper>
+      <S.Table data-testid={testId}>
+        <thead>
+          <th>Name</th>
+          <th>Value</th>
+          <th>Category</th>
+          <th>Date</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>José Oswaldo dos Santos</td>
+            <td>1000</td>
+            <td>Mindingo</td>
+            <td>10/02/2021</td>
+          </tr>
+          <tr>
+            <td>AACD</td>
+            <td>1000</td>
+            <td>ONG</td>
+            <td>10/02/2021</td>
+          </tr>
+          <tr>
+            <td>Agnaldo Gonçalves</td>
+            <td>1000</td>
+            <td>Mindingo</td>
+            <td>10/02/2021</td>
+          </tr>
+          <tr>
+            <td>Associação dos anões Cientistas</td>
+            <td>1000</td>
+            <td>Instituto de Pesquisa</td>
+            <td>10/02/2021</td>
+          </tr>
+        </tbody>
+      </S.Table>
     </S.Container>
   )
 }
--- packages/shared-ui/src/lib/components/money/TransactionTable/TransactionTable.styles.ts
+++ packages/shared-ui/src/lib/components/money/TransactionTable/TransactionTable.styles.ts
@@ -1,19 +1,26 @@
 import styled, { css } from 'styled-components'
 
-import { TransactionTableProps } from '.'
+// import { TransactionTableProps } from '.'
 
-export const Container = styled.div<TransactionTableProps>`
-  ${({ theme, color }) => css`
-    height: 100px;
-    width: 500px;
-  `}
-`
-
-export const Wrapper = styled.div<TransactionTableProps>`
+export const Container = styled.div`
   ${({ theme }) => css`
-    background-color: red;
+    margin-top: 4rem;
+
+    font-size: 1rem;
+    font-weight: 100;
   `}
 `
-export const Main = styled.h1`
-  color: black;
+
+export const Table = styled.table`
+  ${({ theme }) => css`
+    border-spacing: 0 0.5rem;
+
+    tr:nth-child(even) {
+      background-color: ${theme.colors.gray};
+    }
+
+    tr:hover {
+      background-color: ${theme.colors.danger};
+    }
+  `}
 `
