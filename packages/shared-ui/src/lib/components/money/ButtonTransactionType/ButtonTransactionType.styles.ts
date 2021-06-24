71048aa4be083ed944b5d7860e6dff06ddbc7552
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-24 06:17:53 -0300

    🎨 style: added polished lighten to selected buttons

--- packages/shared-ui/src/lib/components/money/ButtonTransactionType/ButtonTransactionType.styles.ts
+++ packages/shared-ui/src/lib/components/money/ButtonTransactionType/ButtonTransactionType.styles.ts
@@ -1,27 +1,28 @@
 import styled, { css } from 'styled-components'
-import { darken } from 'polished'
+import { darken, lighten } from 'polished'
 import { ButtonTransactionTypeProps } from '.'
 
-type ButtonType = Pick<ButtonTransactionTypeProps, 'transactionType'>
+type ButtonType = Omit<ButtonTransactionTypeProps, 'testId'>
 
 export const ButtonTransaction = styled.button<ButtonType>`
-  ${({ theme, transactionType }) => css`
+  ${({ theme, isSelected, transactionType }) => css`
     display: flex;
     align-items: center;
     justify-content: center;
     font-size: 1.2rem;
+    height: 4rem;
 
     color: ${transactionType === 'income' ? 'green' : 'red'};
 
-    background: transparent;
-    border: 2px solid ${theme.colors.mindingoBorder};
+    background: ${isSelected
+      ? lighten(0.4, transactionType === 'income' ? 'green' : 'red')
+      : 'transparent'};
 
-    height: 4rem;
+    border: 2px solid ${theme.colors.mindingoBorder};
 
     transition: ${theme.transition.default};
 
     &:hover {
-      /* filter: brightness(50%); */
       border-color: ${darken(0.2, theme.colors.mindingoBorder)};
     }
 
