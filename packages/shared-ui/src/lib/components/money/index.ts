ab5ae3b2afcd37bc6246bf0329d6ae07f6151c68
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-26 05:11:31 -0300

    🐛 fix: fixed build

--- packages/shared-ui/src/lib/components/money/index.ts
+++ packages/shared-ui/src/lib/components/money/index.ts
@@ -1,15 +1,13 @@
 import * as Header from './Header'
+import * as NewTransactionModal from './NewTransactionModal'
 import * as SummaryCard from './SummaryCard'
 import * as TransactionTable from './TransactionTable'
-import * as NewTransactionModal from './NewTransactionModal'
-import * as ButtonTransactionType from './ButtonTransactionType'
 
 const money = {
   ...Header,
   ...SummaryCard,
   ...TransactionTable,
-  ...NewTransactionModal,
-  ...ButtonTransactionType
+  ...NewTransactionModal
 }
 
 export { money }
