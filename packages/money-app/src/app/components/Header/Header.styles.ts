01e5da910ecd991dde8459776473d0f6c1533c08
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-26 07:36:57 -0300

    🎨 style: added linear gradient to header

--- packages/money-app/src/app/components/Header/Header.styles.ts
+++ packages/money-app/src/app/components/Header/Header.styles.ts
@@ -5,7 +5,17 @@ import { FcMoneyTransfer } from 'react-icons/fc'
 
 export const Container = styled.header`
   ${({ theme }) => css`
-    background-color: ${theme.colors.mindingoBlue};
+    background: #8e2de2; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      #4a00e0,
+      #8e2de2
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to right,
+      #4a00e0,
+      #8e2de2
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   `}
 `
 
@@ -32,7 +42,9 @@ export const Button = styled.button`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.xsmall};
     color: ${theme.colors.mindingoShape};
-    background-color: ${theme.colors.mindingoBlueLight};
+
+    background: #5d26c1;
+
     border: 0;
     padding: 0 2rem;
     border-radius: 0.25rem;
