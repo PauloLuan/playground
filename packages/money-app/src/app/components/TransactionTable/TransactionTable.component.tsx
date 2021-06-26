b70f917246cb3d0f061ab096099b082aa94b834a
Paulo Luan <pauloluan.inova@gmail.com>
2021-06-26 07:27:08 -0300

    🎨 style: added flexible mocka data and format price and date on fields

--- packages/money-app/src/app/components/TransactionTable/TransactionTable.component.tsx
+++ packages/money-app/src/app/components/TransactionTable/TransactionTable.component.tsx
@@ -4,9 +4,67 @@ export interface TransactionTableProps {
   testId?: string
 }
 
+export type TransactionType = {
+  _id: string | number
+  name: string
+  amount: number
+  category?: 'Mindingo' | 'ONG' | 'Institute'
+  transactionCategory: 'income' | 'donation'
+  createdAt: Date
+}
+
 export const TransactionTable = ({
   testId = 'TransactionTable'
 }: TransactionTableProps) => {
+  const data: TransactionType[] = [
+    {
+      _id: 1,
+      name: 'Rich Donator',
+      amount: 12865,
+      transactionCategory: 'income',
+      createdAt: new Date('10/02/2021')
+    },
+    {
+      _id: 2,
+      name: 'José Oswaldo dos Santos',
+      amount: 789,
+      category: 'Mindingo',
+      transactionCategory: 'donation',
+      createdAt: new Date('10/02/2021')
+    },
+    {
+      _id: 3,
+      name: 'AACD',
+      amount: 337,
+      category: 'ONG',
+      transactionCategory: 'donation',
+      createdAt: new Date('10/02/2021')
+    },
+    {
+      _id: 4,
+      name: 'Agnaldo Gonçalves',
+      amount: 596,
+      category: 'Mindingo',
+      transactionCategory: 'donation',
+      createdAt: new Date('10/02/2021')
+    },
+    {
+      _id: 5,
+      name: 'Received',
+      amount: 2987,
+      transactionCategory: 'income',
+      createdAt: new Date('10/02/2021')
+    },
+    {
+      _id: 6,
+      name: 'Associação dos anões Cientistas',
+      amount: 3654,
+      category: 'Institute',
+      transactionCategory: 'donation',
+      createdAt: new Date('10/02/2021')
+    }
+  ]
+
   return (
     <S.Container>
       <S.Table data-testid={testId}>
@@ -19,42 +77,21 @@ export const TransactionTable = ({
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Received</td>
-            <td className="income">12865</td>
-            <td>Income</td>
-            <td>10/02/2021</td>
-          </tr>
-          <tr>
-            <td>José Oswaldo dos Santos</td>
-            <td className="donation">789</td>
-            <td>Mindingo</td>
-            <td>10/02/2021</td>
-          </tr>
-          <tr>
-            <td>AACD</td>
-            <td className="donation">337</td>
-            <td>ONG</td>
-            <td>10/02/2021</td>
-          </tr>
-          <tr>
-            <td>Agnaldo Gonçalves</td>
-            <td className="donation">596</td>
-            <td>Mindingo</td>
-            <td>10/02/2021</td>
-          </tr>
-          <tr>
-            <td>Received</td>
-            <td className="income">2987</td>
-            <td>Income</td>
-            <td>10/02/2021</td>
-          </tr>
-          <tr>
-            <td>Associação dos anões Cientistas</td>
-            <td className="donation">3654</td>
-            <td>Instituto de Pesquisa</td>
-            <td>10/02/2021</td>
-          </tr>
+          {data.map((data) => (
+            <tr key={data._id}>
+              <td>{data.name}</td>
+              <td className={data.transactionCategory}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(data.amount)}
+              </td>
+              <td className={data.transactionCategory}>
+                {data.transactionCategory}
+              </td>
+              <td>{data.createdAt.toLocaleDateString('pt-BR')}</td>
+            </tr>
+          ))}
         </tbody>
       </S.Table>
     </S.Container>
