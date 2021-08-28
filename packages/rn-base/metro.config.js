9e6754d12da0918ba5fec359d97e015464c0c730
Paulo Luan <pauloluan.inova@gmail.com>
2021-08-28 07:15:46 -0300

    🔧 chore: web is working, android not yet...

--- packages/rn-base/metro.config.js
+++ packages/rn-base/metro.config.js
@@ -1,3 +1,33 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces')
+const path = require('path')
+const getWorkspaces = require('get-yarn-workspaces')
+const workspaces = getWorkspaces(__dirname)
+const blacklist = require('metro-config/src/defaults/exclusionList')
 
-module.exports = createMetroConfiguration(__dirname)
+module.exports = {
+  projectRoot: path.resolve(__dirname, '.'),
+  // watchFolders: [path.resolve(__dirname, './node_modules')],
+  watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces],
+
+  resolver: {
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/
+    ]),
+
+    // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => path.join(process.cwd(), `node_modules/${name}`)
+      }
+    )
+  },
+
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  }
+}
