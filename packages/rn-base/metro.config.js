f206351062ad69cfbd3d026b2c9fb4ffb5a14527
Paulo Luan <pauloluan.inova@gmail.com>
2021-09-03 04:26:37 -0300

    🔧 chore: trying to make expo-yarn-workspaces works

--- packages/rn-base/metro.config.js
+++ packages/rn-base/metro.config.js
@@ -1,33 +1,3 @@
-const path = require('path')
-const getWorkspaces = require('get-yarn-workspaces')
-const workspaces = getWorkspaces(__dirname)
-const blacklist = require('metro-config/src/defaults/exclusionList')
+const { createMetroConfiguration } = require('expo-yarn-workspaces')
 
-module.exports = {
-  projectRoot: path.resolve(__dirname, '.'),
-  // watchFolders: [path.resolve(__dirname, './node_modules')],
-  watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces],
-
-  resolver: {
-    blacklistRE: blacklist([
-      /node_modules\/.*\/node_modules\/react-native\/.*/
-    ]),
-
-    // https://github.com/facebook/metro/issues/1#issuecomment-453450709
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(process.cwd(), `node_modules/${name}`)
-      }
-    )
-  },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
-  }
-}
+module.exports = createMetroConfiguration(__dirname)
